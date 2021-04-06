b110e10a20e8183c033cfa43eefe8fcea106f9ed
Jhon Oliver <johnoliver021@gmail.com>
2021-04-06 11:56:04 -0300

    continuing implementation of beer card

--- src/components/BeerCard/index.tsx
+++ src/components/BeerCard/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Document } from 'prismic-javascript/types/documents';
-import { Container } from './styles';
+import { Container, ContainerImg, Footer } from './styles';
 import PrismicDom from 'prismic-dom';
+import { FaRecycle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 interface BeerProps {
   beer: Document;
@@ -10,8 +12,23 @@ interface BeerProps {
 const BeerCard: React.FC<BeerProps> = ({ beer }) => {
   return (
     <Container>
-      {' '}
-      <h1>{PrismicDom.RichText.asText(beer.data.name)}</h1>
+      <div>
+        <ContainerImg>
+          <img src={beer.data.image.url} />
+        </ContainerImg>
+        <h1>{PrismicDom.RichText.asText(beer.data.name)}</h1>
+        <h2>
+          {beer.data.quantity} Unit • {beer.data.ml}{' '}
+          {PrismicDom.RichText.asText(beer.data.description)}
+          {beer.data.recyclable && <FaRecycle size={10} color="blue" />}
+        </h2>
+        <h3>RD${beer.data.price}.00/Unit</h3>
+        <h4>{PrismicDom.RichText.asText(beer.data.promotiondescription)}</h4>
+        <Link to="">View Details</Link>
+      </div>
+      <Footer>
+        <p>footer</p>
+      </Footer>
     </Container>
   );
 };
--- src/components/BeerCard/styles.ts
+++ src/components/BeerCard/styles.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: space-between;
   cursor: pointer;
   width: 300px;
-  height: 300px;
+  height: 400px;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 1px 1px;
   margin-right: 15px;
   margin-bottom: 15px;
@@ -11,4 +14,47 @@ export const Container = styled.div`
   h1 {
     font-size: 16px;
   }
+  h2 {
+    font-size: 14px;
+    font-weight: 400;
+    color: #8e8b8b;
+    margin-top: 2px;
+    & svg {
+      margin-left: 5px;
+    }
+  }
+  h3 {
+    font-size: 14px;
+    font-weight: 500;
+    color: #333;
+    margin-top: 5px;
+  }
+  h4 {
+    font-size: 13px;
+    font-weight: 500;
+    color: #5aad45;
+    margin-top: 1rem;
+    text-transform: capitalize;
+  }
+  img {
+    width: 50px;
+    height: 100px;
+  }
+  a {
+    color: blue;
+    font-size: 13px;
+    font-family: Roboto;
+    font-weight: 700;
+  }
 `;
+export const ContainerImg = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1rem;
+  img {
+    width: 80px;
+    height: 120px;
+  }
+`;
+
+export const Footer = styled.div``;
